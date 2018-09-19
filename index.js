0885c9d48f07e185311fdef6f2a03df7d1ecf3da
mccaetano <marcelo.cheruti@gmail.com>
2018-09-19 01:36:40 -0300

    ajustes

--- index.js
+++ index.js
@@ -1,14 +1,6 @@
-var http = require("http");
-
-http.createServer(function (request, response) {
-    // Send the HTTP header 
-    // HTTP Status: 200 : OK
-    // Content Type: text/plain
-    response.writeHead(200, {'Content-Type': 'text/plain'});
-    
-    // Send the response body as "Hello World"
-    response.end('Hello World\n');
- }).listen(8080);
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
  
- // Console will print the message
- console.log('Server running at http://127.0.0.1:8080/');
+server.listen(server_port, server_ip_address, function () {
+  console.log( "Listening on " + server_ip_address + ", port " + server_port )
+});
