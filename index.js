3fd8706ad0ad8aadb1ede176ed7f238cc79b4f55
Marcelo  Cheruti Caetano <marcelo.cheruti@gmail.com>
2018-09-19 10:17:00 -0300

    Update index.js
    alteracao

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
  
 server.createServer(function (req, res) {
-    res.write('Hello World!'); //write a response to the client
+    res.write('Hello World! 1234'); //write a response to the client
     res.end(); //end the response
 }).listen(server_port, function () {
   console.log( "Listening on " + server_ip_address + ", port " + server_port )
