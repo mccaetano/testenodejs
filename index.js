3bb83595e68b8e66a18d1756cb52ae749f7557e3
Marcelo  Cheruti Caetano <marcelo.cheruti@gmail.com>
2018-09-19 11:23:03 -0300

    Update index.js
    teste js dinamico

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
  
 server.createServer(function (req, res) {
-    res.write('Hello World! 1234'); //write a response to the client
+    res.setHeader('content-type', 'application/javascript');
+    res.write('<script> var teste="teste"; </script>'); //write a response to the client
     res.end(); //end the response
 }).listen(server_port, function () {
   console.log( "Listening on " + server_ip_address + ", port " + server_port )
